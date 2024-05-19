2f00757bc07f5770aa51590d17fc6525d1e35345
Bohdan Bodakva <bodakvabohdan44@gmail.com>
2024-05-19 13:01:45 +0300

    added folder for requests handling

---
+++ my-react-app/src/requests/Request.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Request{
+    static BACKEND_ADDRESS = "http://localhost:8080"
+
+    static sendRequest(){
+        
+    }
+}
