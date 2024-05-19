2b7e5c7283549cba79d8ce536e258d3f6fdafc43
TopWork0ut <olegiksen@gmail.com>
2024-05-19 06:58:42 +0300

    add home page animation stylings

--- my-react-app/src/Elements/General/ClassicButton.styled.jsx
+++ my-react-app/src/Elements/General/ClassicButton.styled.jsx
@@ -1,22 +1,24 @@
 import styled from "styled-components";
 
-export  const ClassicButton = styled.button`
-        padding: 1em;
-        border: 2px solid white;
-        border-radius: 5px;
-        background : transparent;
-        text-decoration: none;
-        color: white;
-        font-size: large;
-        padding: 0.5em 2em;
-        cursor: pointer;
-        
-        :hover {
-            transition: 0.3s;
-            color: #062F4F;
-            background-color:white;
-            border: 2px solid lightgrey;
-            box-shadow: 0 1px 0.1em white;
-        }
-        
-`
+// Styled component for the ClassicButton
+export const ClassicButton = styled.button`
+  padding: 1em;
+  border: 2px solid white;
+  border-radius: 5px;
+  background-color: #e3a805;
+  text-decoration: none;
+  color: #093650;
+  font-size: large;
+  padding: 0.5em 2em;
+  cursor: pointer;
+  position: relative;
+  transition: background 0.3s ease;
+  font-weight: bold;
+  border-color: #093650;
+
+  &:hover {
+    color: #f5f2eb;
+    background-color: #093650;
+    border-color: #f5f2eb;
+  }
+`;
